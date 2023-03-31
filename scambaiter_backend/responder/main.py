8f2fabf7ed6b51a6ad6dd335de9c6f9fb7abc6d2
an19352 <piyushbajaj71@gmail.com>
2023-04-01 00:34:38 +0100

    Update main.py

--- scambaiter_backend/responder/main.py
+++ scambaiter_backend/responder/main.py
@@ -1,4 +1,6 @@
-from responder import get_replier_randomly, get_replier_by_name
+# from responder import get_replier_randomly, get_replier_by_name
+
+from . import get_replier_randomly, get_replier_by_name
 
 #for _ in range(100):
 print(get_replier_by_name("Template").get_reply("test"))
