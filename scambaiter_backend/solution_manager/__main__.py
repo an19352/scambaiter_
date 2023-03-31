7587331dc8ca302afb76d5c15d9f26166dc6322c
an19352 <piyushbajaj71@gmail.com>
2023-04-01 00:04:31 +0100

    Revert "3"
    This reverts commit 81da95ed69242f050282ca1efdbd08f3c6626024.

--- scambaiter_backend/solution_manager/__main__.py
+++ scambaiter_backend/solution_manager/__main__.py
@@ -1,11 +1,9 @@
-#from solution_manager import gen_new_addr, addr_exists, scam_exists, get_stored_info #, get_sol_name
-# from .storer import addr_exists, scam_exists, get_stored_info
-from .gen import get_random_addr
+from solution_manager import gen_new_addr, addr_exists, scam_exists, get_stored_info #, get_sol_name
 
 # gen_new_addr("an19352@bristol.ac.uk", "sol1")
 # gen_new_addr("wentaochens@outlook.com", "sol2")
 
-# print(scam_exists("cj27795@mail.sdchaos.top"))
+print(scam_exists("cj27795@mail.sdchaos.top"))
 # print(addr_exists("cj27795@mail.sdchaos.top"))
 # print(get_sol_name("cj27795@mail.sdchaos.top"))
 
