ad3283b3a1c9f9e4f6b388f8562c89ab0b6d24d2
an19352 <piyushbajaj71@gmail.com>
2023-04-01 01:00:02 +0100

    Update __init__.py

--- scambaiter_backend/responder/__init__.py
+++ scambaiter_backend/responder/__init__.py
@@ -3,7 +3,7 @@ import os
 from collections import namedtuple, defaultdict
 
 from secret import MODEL_HISTORY_PATH
-from .replier import Replier, TemplateReplier # NeoEnronReplier, NeoRawReplier, ClassifierReplier,
+from replier import Replier, TemplateReplier # NeoEnronReplier, NeoRawReplier, ClassifierReplier,
 
 replier_list = [TemplateReplier()] # [ClassifierReplier(), NeoEnronReplier(), NeoRawReplier()]
 
