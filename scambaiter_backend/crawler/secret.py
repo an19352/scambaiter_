14864caf6150dacbdbdd37c63e22b676198a28ff
an19352 <piyushbajaj71@gmail.com>
2023-04-02 20:20:54 +0100

    Update secret.py

--- scambaiter_backend/crawler/secret.py
+++ scambaiter_backend/crawler/secret.py
@@ -1,3 +1,3 @@
 CRAWLER_PROG_DIR = "./cache" # has crawled stuff
-MAX_PAGE = 5 # keep 5 instead of 20
+MAX_PAGE = 1 # keep 5 instead of 20
 MAIL_SAVE_DIR = "./emails/queued" # crawled and received emails
