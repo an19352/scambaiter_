5bd08c5c7f07d370496229dd7bb6a88421d774d4
an19352 <piyushbajaj71@gmail.com>
2023-04-04 22:04:50 +0100

    31

--- scambaiter_backend/responder/classifier.py
+++ scambaiter_backend/responder/classifier.py
@@ -4,14 +4,14 @@ from flair.models import TextClassifier
 from secret import CLASSIFIER_PATH
 
 
-def classify(content) -> str:
-    classifier = TextClassifier.load(CLASSIFIER_PATH)
-    sentence = Sentence(content)
-    classifier.predict(sentence)
-
-    label = sentence.get_label("topic")
-    print(f"Classifier result: {label.value} ({label.score})")
-    return label.value.upper()
-
-
-print(classify("Hello"))
+# def classify(content) -> str:
+#     classifier = TextClassifier.load(CLASSIFIER_PATH)
+#     sentence = Sentence(content)
+#     classifier.predict(sentence)
+#
+#     label = sentence.get_label("topic")
+#     print(f"Classifier result: {label.value} ({label.score})")
+#     return label.value.upper()
+#
+#
+# print(classify("Hello"))
