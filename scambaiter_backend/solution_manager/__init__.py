1aee180f1260e32903db52d5c3a02325faf3e41d
an19352 <piyushbajaj71@gmail.com>
2023-04-05 16:17:25 +0100

    36

--- scambaiter_backend/solution_manager/__init__.py
+++ scambaiter_backend/solution_manager/__init__.py
@@ -1,9 +1,9 @@
-from storer import store_addr, addr_exists, get_stored_info, scam_exists
-from gen import get_random_addr
+from .storer import store_addr, addr_exists, get_stored_info, scam_exists
+from .gen import get_random_addr
 
 def gen_new_addr(scam_email, sol_name):
     new_addr = get_random_addr()
     store_addr(new_addr, scam_email, sol_name)
     return new_addr
 
-print(gen_new_addr("piyushbajaj71@gmail.com","Template"))
+# print(gen_new_addr("piyushbajaj71@gmail.com","Template"))
--- scambaiter_backend/solution_manager/__main__.py
+++ scambaiter_backend/solution_manager/__main__.py
@@ -1,14 +1,14 @@
 #from solution_manager import gen_new_addr, addr_exists, scam_exists, get_stored_info #, get_sol_name
 
-from storer import addr_exists, scam_exists, get_stored_info
+# from storer import addr_exists, scam_exists, get_stored_info
 #from gen import get_random_addr
 
 # gen_new_addr("piyushbajaj71@gmail.com", "Template")
 # gen_new_addr("wentaochens@outlook.com", "sol2")
 
-print(scam_exists("piyushbajaj71@gmail.com"))
+# print(scam_exists("piyushbajaj71@gmail.com"))
 # print(addr_exists("an19345@liamacs.email"))
 # print(get_sol_name("cj27795@mail.sdchaos.top"))
 
-store = get_stored_info("xn14328@liammacs.email", "piyushbajaj71@gmail.com")
-print(store)
+# store = get_stored_info("xn14328@liammacs.email", "piyushbajaj71@gmail.com")
+# print(store)
