8ac194740cd5f7ce78a4b05013e22499dc5bf7fe
an19352 <piyushbajaj71@gmail.com>
2023-04-07 01:25:52 +0100

    47

--- scambaiter_backend/responder/Chatgpt_Replier.py
+++ scambaiter_backend/responder/Chatgpt_Replier.py
@@ -1,15 +1,20 @@
 import os
 import openai
 from secret import OPENAI_API_KEY
+# from secret import CONTENT
 
 openai.api_key = OPENAI_API_KEY # os.getenv("OPENAI_API_KEY")
 
 def gen_text1(prompt):
+  prompt = "Reply without any signature :" + prompt
   completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo-0301",
+    model="gpt-3.5-turbo",
     messages=[
+      {"role": "system", "content": "You are a dull person and gullible person who is easy to persuade. You want to share information. Do not call them a scam. Keep conversation going. Ask follow-up questions. Do not give any personal information. Greet them in the start."},
       {"role": "user", "content": prompt}
-    ]
+    ],
+    temperature = 0.2,
+    top_p = 0.2
   )
   res = completion.choices[0].message.content
   return res
@@ -18,7 +23,7 @@ def gen_text1(prompt):
 
 def gen_text2(prompt):
   completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo-0301",
+    model="gpt-3.5-turbo",
     messages=[
       {"role": "user", "content": prompt}
     ]
@@ -28,4 +33,4 @@ def gen_text2(prompt):
   # print(res)
 
 
-# gen_text2("Hello")
+# print(gen_text1(CONTENT))
