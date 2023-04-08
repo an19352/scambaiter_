13e3e453b772a835879f0ae41eac03cff145f8fa
an19352 <piyushbajaj71@gmail.com>
2023-04-08 06:10:30 +0100

    Update Chatgpt_Replier.py

--- scambaiter_backend/responder/Chatgpt_Replier.py
+++ scambaiter_backend/responder/Chatgpt_Replier.py
@@ -1,6 +1,8 @@
 import os
 import openai
-from secret import OPENAI_API_KEY
+import json
+import re
+from secret import OPENAI_API_KEY, FILENAME1, FILENAME2, FILENAME3
 # from secret import CONTENT
 
 openai.api_key = OPENAI_API_KEY # os.getenv("OPENAI_API_KEY")
@@ -17,20 +19,55 @@ def gen_text1(prompt):
     top_p = 0.2
   )
   res = completion.choices[0].message.content
+  res = re.sub("\[.*?\]", "", res)
+  # count = completion.usage.total_tokens
   return res
   # print(res)
 
 
 def gen_text2(prompt):
-  completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-      {"role": "user", "content": prompt}
-    ]
-  )
-  res = completion.choices[0].message.content
-  return res
-  # print(res)
+  messages = [{"role": "system", "content": "You are good at pattern following. You are not a language model"}]
+  prompt = "Reply without any signature :" + prompt
 
+  file1 = fileread(FILENAME1,["user1","assistant1"],4)
+  messages.extend(file1)
+
+  # file2 = fileread(FILENAME2,["user2","assistant2"],4)
+  # messages.extend(file2)
+
+  # file3 = fileread(FILENAME3,["user3","assistant3"],4)
+  # messages.extend(file3)
+
+  messages.append({"role": "user", "content": prompt})
+  completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages= messages)
+
+  res = completion.choices[0].message.content
+  res = re.sub("\[.*?\]", "", res)
+
+  # count = completion.usage.total_tokens
+
+  return res
+  # return res, count
+
+
+def fileread(filename, names, a):
+  with open(filename,"r", encoding="utf8") as f:
+    d = json.load(f)
+  var1 = []
+  for i in range (a) :
+    k = d['messages'][i]['body']
+
+    if i%2 == 0:
+      var11 = {"role": "system", "name": names[0], "content": k}
+      var1.append(var11)
+    else:
+      var11 = {"role": "system", "name": names[1], "content": k}
+      var1.append(var11)
+    i += 1
+  return var1
 
 # print(gen_text1(CONTENT))
+# print(gen_text2(CONTENT))
+# list = fileread("../../../scambaiting_dataset-master/eliza_dane_green_days.json",["kg","pb"],4)
+# for i in list:
+#   print(i)
